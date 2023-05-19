a51085e1f358efa5e22140bc0771dde7cf59d70e
alexander.schranner <alexader.schranner@rwth-aachen.de>
2023-05-19 19:49:51 +0100

    Inital commit for branch

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,23 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Ionic-StuddyBuddy',
+  appId: 'es.ulpgc.ionic.angular',
+  appName: 'IonicAngularApp',
   webDir: 'www',
-  server: {
-    androidScheme: 'https'
+  bundledWebRuntime: false,
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      'android-minSdkVersion': '19',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '0',
+      GOOGLE_MAPS_ANDROID_API_KEY: 'AIzaSyCz4WvFFbwV_d2neqTmiXVN-bsfbsS2lgw',
+      GOOGLE_MAPS_IOS_API_KEY: 'AIzaSyCz4WvFFbwV_d2neqTmiXVN-bsfbsS2lgw'
+    }
   }
 };
 
