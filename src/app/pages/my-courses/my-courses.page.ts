79da3f71692047cb1ccb9e8e2304ddafa2e81485
alexander.schranner <alexader.schranner@rwth-aachen.de>
2023-05-23 12:31:31 +0100

    Add functionlaity: Load courses according to user

--- src/app/pages/my-courses/my-courses.page.ts
+++ src/app/pages/my-courses/my-courses.page.ts
@@ -5,6 +5,7 @@ import { DatabaseService } from "../../services/database.service";
 import { Course } from "src/app/models/course";
 import { DbFirebaseService } from "src/app/services/db-firebase.service";
 import { DbSqliteService } from "src/app/services/db-sqlite.service";
+import { AuthService } from "src/app/services/auth-service.service";
 
 @Component({
   selector: "app-my-courses-list",
@@ -19,9 +20,10 @@ export class MyCoursesPage implements OnInit {
 
   constructor(
     //private db: DatabaseService,
-    private courseService: CourseService,
-    //private dbFS: DbFirebaseService,
-    private dbSQL: DbSqliteService
+    //private courseService: CourseService,
+    private auth: AuthService,
+    private dbFS: DbFirebaseService,
+    //private dbSQL: DbSqliteService
   ) {
   }
 
@@ -31,18 +33,38 @@ export class MyCoursesPage implements OnInit {
   } */
 
   ngOnInit() {    
-    this.dbSQL.getDatabaseState().subscribe((dbReady) => {
+    this.getCourses();
+    /*this.dbSQL.getDatabaseState().subscribe((dbReady) => {
       if (dbReady) {
         this.getCourses();
       }
     }); 
-    this.getCourses();
+    this.getCourses();*/
   }
 
-  getCourses(): void {
-    this.courseService.getAllCourses().subscribe((courses) => {
+  async getCourses() {
+    /*this.courseService.getAllCourses().subscribe((courses) => {
       this.courses = courses;
-    });
+    });*/
+    const username = await this.auth.getCurrentUserName();
+    const userDocument = await this.dbFS.getUserData(username);
+    let usersCourses = [];
+    if (userDocument.exists) {
+      usersCourses = userDocument.data()['courses'];
+    }
+    for (let i = 0; i < usersCourses.length; i++) {
+      const courseDocument = await this.dbFS.getCourseById(usersCourses[i])
+      if (courseDocument.exists) {
+        const course: Course = {
+          id: courseDocument.data()['id'],
+          title: courseDocument.data()['title'],
+          description: courseDocument.data()['description'],
+          createdByUserID: courseDocument.data()['createdByUserID'],
+          imageURL: courseDocument.data()['imageURL']
+        }
+        this.courses.push(course);
+      }
+    }
   }
 
   /*toggleFavorite(course: Course): void {
